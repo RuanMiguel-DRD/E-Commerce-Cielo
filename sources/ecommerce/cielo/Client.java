cd97ed9308cb4e1eddc58310cf329275ba88fcc0
Ruan Miguel <ruanmigueldrd@outlook.com>
2026-08-05 15:24:27 -0300

    feat (core): ✨ Adição da classe Client
    - Adicionado a classe Client, que serve como classe principal e central da biblioteca
    
    A classe Client intermedia a classe Query e a Transactional, permitindo o uso de qualquer função da API através da mesma
    
    A classe também gera configurações padrões e aplica validações importantes, para garantir o funcionamento da biblioteca, e facilitar o trabalho de quem estiver usando a biblioteca
    
    Apesar de facilitar o desenvolvimento e ser a parte central da biblioteca, é possível usar a classe Query e Transactional diretamente, sem a necessidade da classe Client, apesar disso não ser uma boa prática

---
+++ sources/ecommerce/cielo/Client.java
@@ -0,0 +1,53 @@
+package ecommerce.cielo;
+
+
+import lombok.NonNull;
+
+import java.net.http.HttpClient;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.Builder;
+
+import java.time.Duration;
+
+import ecommerce.cielo.api.Query;
+import ecommerce.cielo.api.Transactional;
+
+import ecommerce.cielo.api.web.Network;
+
+
+public class Client {
+
+    public final Query query;
+    public final Transactional transactional;
+
+    public Client(@NonNull Credentials auth, @NonNull Environment env) {
+        this(auth, env, null);
+    }
+
+    public Client(@NonNull Credentials auth, @NonNull Environment env, HttpClient client) {
+
+        // Cliente HTTP básico/padrão
+        if (client == null) {
+            client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+        }
+
+        // Cabeçalho HTTP universal
+        Builder request = HttpRequest.newBuilder()
+            .headers(
+                "Accept", "application/json",
+                "Content-Type", "application/json",
+                "MerchantId", auth.getMerchantId(),
+                "MerchantKey", auth.getMerchantKey()
+            );
+
+            Network net = new Network(client, request);
+
+        this.query = new Query(net, env.getUrlQuery());
+        this.transactional = new Transactional(net, env.getUrlTransactional());
+
+    }
+
+}
